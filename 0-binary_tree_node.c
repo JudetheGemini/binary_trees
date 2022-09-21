34d73b91ffa8a3405a17ef3022c5857839620b27
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-09-21 08:46:29 +0100

    adding c file that creates a node in a binary tree [betty passed]

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,14 +1,16 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ * binary_tree_node - create a node in binary tree
+ * @parent: parent of node
+ * @value: value held in node
+ * Return: address of new node created
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
 	newNode->parent = parent;
 	newNode->n = value;
-	return newNode;
+	return (newNode);
 }
