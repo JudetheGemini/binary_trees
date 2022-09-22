1b91d1599813a49b55e47ac7b260d22caf42a39d
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-09-22 11:30:55 +0100

    second fix for 0-binary_tree_node.c [betty passed]

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,12 +10,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-	if (newNode)
-	{
-		newNode->parent = parent;
-		newNode->n = value;
-		return (newNode);
-	}
-	else
+	if (newNode == NULL)
 		return (NULL);
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return (newNode);
 }
